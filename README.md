85d37154f21b57d00386720f71fe380605faeb3f
Thomas <t.deruel@hotmail.fr>
2018-02-24 01:45:07 +0100

    huge update

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
-# portfolio-V2 Mobile First
- Section Home :white_check_mark: Responsive :black_square_button: Compatibilité :moz :white_check_mark: chrome :white_check_mark:
- 
- Section Menu :white_check_mark: Responsive :black_square_button: Compatibilité :moz :white_check_mark: chrome :white_check_mark:
- 
- Section Works :black_square_button: Respnsive :black_square_button:
- 
- Section Detail :black_square_button: Responsive :black_square_button:
- 
- Section About :black_square_button: Responsive :black_square_button:
+﻿# portfolio-V2 Mobile First
+
+## Partie HTML/CSS
+ Section Home :white_check_mark: Responsive :white_check_mark: Compatibilité :moz :white_check_mark: chrome :white_check_mark:
+
+ Section Menu :white_check_mark: Responsive :white_check_mark: Compatibilité :moz :white_check_mark: chrome :white_check_mark:
+
+ Section Works :white_check_mark: Responsive :black_square_button:
+
+ Section Detail :white_check_mark: Responsive :white_check_mark: [Link](https://codepen.io/ThomasDeruel/pen/YevLMO)
+
+ Section About :white_check_mark: Responsive :black_square_button:
+
+---------------------------------
+## Partie JS
+
+ Section Home :white_check_mark:
+
+ Section Menu :white_check_mark: 
+
+ Section Works :black_square_button:
+
+ Section Detail :white_check_mark: 
+
+ Section About :black_square_button:
+
+Creation + Gestion Data :black_square_button:
+
+Animation scroll :black_square_button:
+
--- script/animation.js
+++ script/animation.js
@@ -1,7 +1,10 @@
 var menudots = document.querySelectorAll('.menuItems');
 var menu = document.querySelector('.logomenu');
+var sectionMenu = document.querySelector('.menu')
 var click=false;
-menu.addEventListener('click',function(){
+var fixed = false;
+var dotsAnimation = function(){
+  menu.classList.add('fixed');
   menudots[0].classList.toggle('croix-opacity');
   menudots[1].classList.toggle('croix1');
   menudots[2].classList.toggle('croix-opacity');
@@ -10,16 +13,20 @@ menu.addEventListener('click',function(){
   menudots[5].classList.toggle('croix2');
   menudots[6].classList.toggle('croix-opacity');
   menudots[7].classList.toggle('croix4');
+}
+menu.addEventListener('click',dotsAnimation);
+sectionMenu.addEventListener('click',dotsAnimation);
 
-})
 // MENU //
 var menulist=document.querySelectorAll('.navItems');
+var navContainer = document.querySelector('.navigationContainer')
 var menuAnim=document.querySelectorAll('.navItemsAnimation');
 var listMenu=[document.querySelector('.navItemsPortfolio'),document.querySelector('.navItemsWorks'),document.querySelector('.navItemsAbout')];
 for(let i =0;i<menulist.length;i++)
   {
     menulist[i].addEventListener("animationend", function(){
     this.addEventListener('mouseenter',function(){
+
       this.style.color='rgb(2,2,2,0.10)';
       this.style.color='rgba(2,2,2,0.10)';
       listMenu[i].style.color="#232323";
@@ -27,10 +34,11 @@ for(let i =0;i<menulist.length;i++)
       })
           menulist[i].addEventListener('mouseleave',function(){
       this.style.color='white';
+
       menuAnim[i].style.width="0";
       listMenu[i].style.color="rgb(228,228,228,0.02)";
       listMenu[i].style.color="rgba(228,228,228,0.02)";
-      })
+    });
       });
   }
 
@@ -46,40 +54,36 @@ menulist[0].addEventListener('mouseenter',function(){
       menudots[j].style.background="#232323";
     }
 })
-menu.addEventListener('click',function(){
-
-
+var navigationAnimation = function(){
   if(click===false)
   {
-    click=true;
+    navContainer.style.left="0";
+    navContainer.style.transition="all .3s";
     delay=0;
     for(var i =0;i<menulist.length;i++)
     {
       menulist[i].style.left='-100%';
-    menulist[i].style.animation= "animMenuOn .3s ease-in-out forwards";
+      menulist[i].style.animation= "animMenuOn .3s ease-in-out forwards";
       menulist[i].style.animationDelay=delay+"s";
       delay=delay+.2;
-  }}
+    }
+    click=true;
+  }
   else
   {
-    click=false;
     delay=0;
-    for(var i =0;i<menulist.length;i++)
+    for(var j =0;j<menulist.length;j++)
     {
-      menulist[i].style.left='0';
-      menulist[i].style.animation= "animMenuOff .3s ease-in-out forwards";
-      menulist[i].style.animationDelay=delay+"s";
+      menulist[j].style.left='0';
+      menulist[j].style.animation= "animMenuOff .3s ease-in-out forwards";
+      menulist[j].style.animationDelay=delay+"s";
       delay=delay+.2;
+    }
+        navContainer.style.left="-100%";
+        navContainer.style.transition="all 4s";
+        click=false;
   }
 }
 
-});
-  /*else{
-    click=undefined;
-}
-/*for(var i =0;i<menulist.length;i++)
-{
-  menulist[i].style.animation="animMenuOff .3s ease-in-out forwards";
-}
-}
-})*/
+menu.addEventListener('click',navigationAnimation);
+sectionMenu.addEventListener('click',navigationAnimation);
