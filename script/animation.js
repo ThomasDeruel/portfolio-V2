485d6c91efcadd6d63f32e7d393d4fc0acd6a100
Thomas Deruel <t.deruel@hotmail.fr>
2018-03-27 13:57:14 +0200

    add details

--- script/animation.js
+++ script/animation.js
@@ -13,12 +13,12 @@ var dotsAnimation = function(){
   menudots[5].classList.toggle('croix2');
   menudots[6].classList.toggle('croix-opacity');
   menudots[7].classList.toggle('croix4');
-}
+};
 menu.addEventListener('click',dotsAnimation);
 sectionMenu.addEventListener('click',dotsAnimation);
-
 // MENU //
 var menulist=document.querySelectorAll('.navItems');
+var linklist = document.querySelectorAll('.navItems-link')
 var navContainer = document.querySelector('.navigationContainer')
 var menuAnim=document.querySelectorAll('.navItemsAnimation');
 var listMenu=[document.querySelector('.navItemsPortfolio'),document.querySelector('.navItemsWorks'),document.querySelector('.navItemsAbout')];
@@ -27,21 +27,21 @@ for(let i =0;i<menulist.length;i++)
     menulist[i].addEventListener("animationend", function(){
     this.addEventListener('mouseenter',function(){
 
-      this.style.color='rgb(2,2,2,0.10)';
-      this.style.color='rgba(2,2,2,0.10)';
+      /*this.style.color='rgb(2,2,2,0.10)';
+      this.style.color='rgba(2,2,2,0.10)';*/
+      linklist[i].style.color="rgba(2,2,2,0.10)";
+      linklist[i].style.color="rgb(2,2,2,0.10)";
       listMenu[i].style.color="#232323";
       menuAnim[i].style.width="100%";
       })
           menulist[i].addEventListener('mouseleave',function(){
-      this.style.color='white';
-
+      linklist[i].style.color='#e4e4e4';
       menuAnim[i].style.width="0";
       listMenu[i].style.color="rgb(228,228,228,0.02)";
       listMenu[i].style.color="rgba(228,228,228,0.02)";
     });
       });
   }
-
 menulist[0].addEventListener('mouseleave',function(){
   for(var j=0;j<menudots.length;j++)
     {
@@ -83,39 +83,10 @@ var navigationAnimation = function(){
         navContainer.style.transition="all 4s";
         click=false;
   }
-}
+};
 menu.addEventListener('click',navigationAnimation);
 sectionMenu.addEventListener('click',navigationAnimation);
-/* ANIMATION DETAILS */
-var btnPrev = document.querySelector('.project_previous');
-var btnNext = document.querySelector('.project_next');
-var arrowPrev = document.querySelector('.arrow_previous');
-var titlePrev = document.querySelector('.project_previous_name_title');
-var arrowNext = document.querySelector('.arrow_next');
-var titleNext = document.querySelector('.project_next_name_title');
-
-var btnAnimation = function(arrow,title)
-{
-  arrow.classList.toggle('--action');
-  title.classList.toggle('--action');
-};
-
-btnPrev.addEventListener('mouseover',function(){
-btnAnimation(arrowPrev,titlePrev);
-});
-btnPrev.addEventListener('mouseout',function(){
-btnAnimation(arrowPrev,titlePrev);
-});
-
-btnNext.addEventListener('mouseover',function(){
-btnAnimation(arrowNext,titleNext);
-});
-btnNext.addEventListener('mouseout',function(){
-btnAnimation(arrowNext,titleNext);
-});
-
-
-var works = document.querySelectorAll('.worksItems');
+var works = document.querySelectorAll('.worksItems');//---------------------------------------
 var workslink = document.querySelectorAll('.worksItemsTitle');
 var projectUi = document.querySelector('.project');
 var projectContainer = document.querySelector('.project_container')
@@ -123,15 +94,16 @@ var back = document.querySelector('.project_back');
 var contact = document.querySelector('.contact');
 var about = document.querySelector('.about');
 var index;
-
-//Gérer data
+//Gérer data_date
 for (var i = 0; i < workslink .length; i++) {
  workslink[i].textContent= data[i].name;
   workslink[i].setAttribute('data-date',data[i].date);
 };
-
-
 /* transition worksItems and detail + data */
+var bgBorder = document.querySelectorAll('.bg-border');
+var bg = document.querySelector('.bg');
+var bgMemo = bg.style.background;
+var bgNumber = document.querySelectorAll('.worksItemsNumber');
 var dataTitle = document.querySelector('.data_title');
 var dataBg = document.querySelector('.project_header');
 var dataDate = document.querySelector('.data_date');
@@ -143,6 +115,9 @@ var previous = document.querySelector('.project_previous');
 var next = document.querySelector('.project_next');
 var btnContainer = document.querySelector('.project_button');
 var projectNumb = document.querySelector('.project_number');
+var titlePrev = document.querySelector('.project_previous_name_title');
+var titleNext = document.querySelector('.project_next_name_title');
+var imgs = document.querySelectorAll('.projectimg');
 var myData = function(index)
 {
   if(index === 0)
@@ -165,33 +140,25 @@ var myData = function(index)
   dataTitle.textContent = data[index].name;
   dataDate.textContent = data[index].date;
   dataTools.textContent = data[index].tools;
-  dataContent.textContent =data[index].content;
-  dataLink.textContent = data[index].link;
-  for (let j = 0; j < dataColors.length; j++) {
+  dataContent.innerHTML =data[index].content;
+  dataLink.setAttribute('href',data[index].link);
+  //
+  for (var j = 0; j < dataColors.length; j++) {
     dataColors[j].style.color=data[index].color;
   };
+  for (var k = 0; k < imgs.length; k++) {
+    imgs[k].src = data[index].pictures[k];
+  }
   projectNumb.textContent = "0"+(index+1)+"/"+"0"+(works.length);
+
+  if(index > 0){
+    titlePrev.textContent=data[index-1].name;
+  }
+  if(index < (works.length-1)){
+    titleNext.textContent=data[index+1].name;
+  }
 };
-for (let w = 0; w < works.length; w++) {
- works[w].addEventListener('click',function(){
-   projectUi.style.display="block";
-   projectUi.style.animation="opacity ease 2s forwards";
-   myData(w);
-   //ON RECUPERE L'INDEX;
-   index = w;
-   console.log(index);
- });
- }
-
- back.addEventListener('click',function(){
-   projectUi.style.animation="opacityOff ease 2s forwards";
-   setTimeout(function(){ projectUi.style.display="none"; }, 2000);
- });
-
-var bgBorder = document.querySelectorAll('.bg-border');
-var bg = document.querySelector('.bg');
-var bgNumber = document.querySelectorAll('.worksItemsNumber');
-
+var bgMemo;
 for (let w = 0; w < works.length; w++) {
 var padding = works[w].style.paddingLeft;
  workslink[w].addEventListener('mouseover',function(){
@@ -201,8 +168,8 @@ var padding = works[w].style.paddingLeft;
      workslink[w].setAttribute('data-color',data[w].color);
      bgNumber[w].style.background=data[w].colorBg;
      bg.style.background=data[w].colorBg;
+     bgMemo = data[w].colorBg;
    }
-
   var padInit = works[0].style.paddingLeft;
    for (var j = 0; j < works.length; j++) {
      works[w].style.color=data[w].color;
@@ -215,31 +182,187 @@ var padding = works[w].style.paddingLeft;
        }
      }
    }
-});
-
-workslink[w].addEventListener('mouseout',function(){
+});}
+// mouseout of workslink
+var Worksmouseout = function(){
+  for (var w = 0; w < workslink.length; w++) {
   for (var j = 0; j < works.length; j++) {
     for (var b = 0;b<bgBorder.length; b++) {
       bgBorder[b].style.width="100%";
       bgNumber[w].style.color="#e4e4e4";
       bgNumber[w].style.background="#121212";
-      bg.style.background=data[w].colorBg;
-    }
+      bg.style.background=bgMemo;
+    };
     if(works[j]!==works[w]){
       works[w].style.color="#e4e4e4";
       bgNumber[j].style.background="#121212";
       works[j].style.opacity="1";
       works[j].style.paddingLeft=padding ;
+    };
+  };
+  };
+};
+for (var w = 0; w < workslink.length; w++) {
+  workslink[w].addEventListener('mouseout', Worksmouseout);
+}
+// important ici faire un remove event pour cela, on doit créer une fonction externe de l'event;
+var home = document.querySelector('.home');
+var workTitle = document.querySelector('.worksTitle');
+var worksContainer = document.querySelector('.works');
+var aboutContainer = document.querySelector('.aboutAndContact');
+var elemsAbout = document.querySelectorAll('.about');
+var pic = document.querySelector('.aboutAndContact_Picture');
+var content = document.querySelector('.aboutAndContact_content_container');
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+};
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
     }
-  }
+};
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+};
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+};
+for (let w = 0; w < works.length; w++) {
+ workslink[w].addEventListener('click',function(){
+   disableScroll();
+   myData(w);
+   //ON RECUPERE L'INDEX;
+   index = w;
+   /* animation a faire*/
+   delay=0;
+   home.classList.add('transition');
+   workTitle.removeAttribute('data-aos');
+   worksContainer.removeAttribute('data-aos');
+   pic.removeAttribute('data-aos');
+   content.removeAttribute('data-aos');
+   workTitle.classList.add('transition');
+   // transition all worksItem (opacity 1 to 0 + translate X)
+   //fait disperaitre les block a la fin de l'animation;
+   var transit_to_detail = function(myDelay,container,container2,container3,container4){
+     myDelay = myDelay * 1000;
+     var id = setTimeout(function(){
+
+       projectUi.style.display="block";
+       window.scrollTo(0,0);
+       projectUi.style.animation="opacity ease 1s forwards";
+       projectUi.addEventListener('animationend',function(){
+         container.style.display="none";
+         container2.style.display='none';
+         container3.style.display="none";
+         container4.style.display="none";
+        enableScroll();
+      });
+     },myDelay)
+   };
+   for (var m = 0; m < works.length; m++) {
+    works[m].classList.add('transition2');
+    works[m].style.transitionDelay= delay +'s';
+    delay = delay+.08;
+     }
+    for (var n = 0; n < elemsAbout.length; n++) {
+      elemsAbout[n].classList.add('transition3');
+      elemsAbout[n].style.transitionDelay= delay +'s';
+      delay = delay+.08;
+    }
+  transit_to_detail(delay,worksContainer,aboutContainer,aboutContainer,bg);
 });
+ }
+ back.addEventListener('click',function(){
+   projectUi.style.animation="opacityOff ease 2s forwards";
+   setTimeout(function(){ projectUi.style.display="none"; }, 2000);
+ });
+// bouton smooth
+var smooth = function(){
+  var i = Math.round(window.scrollY);
+  var int = setInterval(function() {
+    window.scrollTo(0, i);
+    i -= 100;
+     console.log(i);
+    if (i === 0 || i<=0)
+    { window.scrollTo (0,0);
+      clearInterval(int);
+    }
+  }, 10);
 }
 // CARROUSEL
 next.addEventListener('click',function(){
   index++;
   myData(index);
+  smooth();
 })
 previous.addEventListener('click',function(){
   index--;
   myData(index);
+  smooth();
+  //window.scrollTo(0,0);
 })
+/* ANIMATION DETAILS */
+var btnPrev = document.querySelector('.project_previous');
+var btnNext = document.querySelector('.project_next');
+var arrowPrev = document.querySelector('.arrow_previous_icon');
+var arrowNext = document.querySelector('.arrow_next_icon');
+var actionBtn = document.querySelectorAll('.--action');
+var btnAnimationNext = function(arrow,title)
+{
+  arrow.style.fill=data[index+1].colorBg;
+  title.style.color=data[index+1].colorBg;
+};
+var btnAnimationBefore = function(arrow,title)
+{
+    arrow.style.fill=data[index-1].colorBg;
+    title.style.color=data[index-1].colorBg;
+};
+var btnAnimationDefaut = function(arrow,title)
+{
+  arrow.style.fill="#ffffff";
+  title.style.color="#ffffff";
+};
+btnPrev.addEventListener('mouseover',function(){
+btnAnimationBefore(arrowPrev,titlePrev);
+});
+btnPrev.addEventListener('mouseout',function(){
+btnAnimationDefaut(arrowPrev,titlePrev);
+});
+btnNext.addEventListener('mouseover',function(){
+btnAnimationNext(arrowNext,titleNext);
+});
+btnNext.addEventListener('mouseout',function(){
+btnAnimationDefaut(arrowNext,titleNext);
+});
+//animation Pic face
+var picFace_one = document.querySelector('.profil_one_bg');
+var picFace_two = document.querySelector('.flex-end');
+var picone = document.querySelector('.profil_one_img');
+var pictwo = document.querySelector('.profil_two_img');
+var pictext = document.querySelector('.profil_two_text_container');
+var picOnMouseOver = function(one,two){
+  one.addEventListener('mouseenter',function(){
+    pictwo.style.opacity="0";
+    pictext.style.opacity="1";
+  })
+  two.addEventListener('mouseenter',function(){
+    pictwo.style.opacity="0";
+    pictext.style.opacity="1";
+  })
+};
+picOnMouseOver(picFace_one,picFace_two);
--- script/data.js
+++ script/data.js
@@ -1,60 +1,77 @@
 var data =
   [{
     name:"Spotnride",
-    content: "Spotnride était notre premier projet pédagogique, et cela fût très enrichissant blablabla",
+    content: "<p>During this intensive week, we had to realise a <b>landing page</b>.</p></br> <p>Our landing did have as theme the urban fashion.</br> So, me and my team decided to put forward a an application which helps people to find spots skatepark<p></br><p>This application is called <b>SpotnRide</b>.</p>",
     date:"20/11/17",
     tools:"html5/css3",
-    link:'none',
-    pictures:['image1','image2','image3'],
-    color:"#ff1ac6",
-    colorBg:"#ff99e6",
-    bgOne:"linear-gradient( rgba(133, 18, 165, .5), rgba(133, 18, 165, .5)), url(https://static4-surfdome.global.ssl.fastly.net/github/com/skate/tiles/skate_clothing.jpg) center no-repeat",
-    bgTwo:"linear-gradient( rgb(133, 18, 165, .5), rgb(133, 18, 165, .5)), url(https://static4-surfdome.global.ssl.fastly.net/github/com/skate/tiles/skate_clothing.jpg) center no-repeat"
+    link:'https://github.com/BillalOI/SpotnRide',
+    pictures:['img/spontridelogo.png','img/spotnridecolors.png','img/spotnrideteaser.jpg'],
+    color:" #fce9e9",
+    colorBg:"#e76565",
+    bgOne:"linear-gradient( rgba(239, 83, 80,.8), rgba(239, 83, 80,.8)), url(img/spotnride-bg.jpg) center no-repeat",
+    bgTwo:"linear-gradient( rgb(239, 83, 80,.8), rgb(239, 83, 80,.8)), url(img/spotnride-bg.jpg) center no-repeat"
   },
   {
     name:"Nespresso",
-    content:"nespresso",
+    content:"<p>It was my first experience as much as <b>Web designer</b>.My mission was to redesign the Nespresso homepage using <b>Photoshop</b>.</p></br><p>The challenge I had here was to redo the design while keeping a coherence with the image of the brand, as was really the first time I did that, I really learned a lot.<p>",
     date:"27/11/17",
     tools:"Photoshop",
     link:"https://ibb.co/hCUtAx",
-    content:"blablablablablablablablablalblalblalblzlblzlblzll",
-    pictures:['image1','image2','image3'],
-    color:"#ff9900",
-    colorBg:"#ffcc80",
-    bgOne:"linear-gradient( rgba(226, 151, 38,.8), rgba(226, 151, 38,.8)), url(https://www.futurebrand.com/uploads/case-studies/_sliderHd/Nespresso-Web-8.jpg) center no-repeat",
-    bgTwo:"linear-gradient( rgb(226, 151, 38,.8), rgb(226, 151, 38,.8)), url(https://www.futurebrand.com/uploads/case-studies/_sliderHd/Nespresso-Web-8.jpg) center no-repeat"
+    pictures:['img/nespresso-1.jpg','img/nespressocolors.png','img/nespresso-3.jpg'],
+    color:"#ffebcc",
+    colorBg:"#ff9900",
+    bgOne:"linear-gradient( rgba(226, 151, 38,.8), rgba(226, 151, 38,.8)), url(img/nespresso-bg.jpg) center no-repeat",
+    bgTwo:"linear-gradient( rgb(226, 151, 38,.8), rgb(226, 151, 38,.8)), url(img/nespresso-bg.jpg) center no-repeat cover"
   },
   {
     name:"Rewind",
-    content:0,
+    content:"<p>This week was the the most rewarding for me. Our mission was to"+
+    "create a streaming service for young creators, dedicated to <b>fans of Fan Film.</b></p></br>"+
+    "<p>Our web application had to contain the following features : "+
+    "<ul><li>create a JavaScript player "+
+    "with a custom design (no library);</li>"+
+    "<li>generate the display of movies from the data variable that is provided in a data.js file;</li>"+
+    "<li>add a design filter of our choice, allowing to filter movies by category, add to this filter the possibility to 'see everything';</li>"+
+    "<li>each film must have a card in the design of our choice, allowing you to see all the details of the chosen film;</li>"+
+    "<li>each movie can be launched / read in the custom player.</li></ul>",
     date:"18/12/17",
-    tools:0,
-    link:0,
-    content:0,
-    pictures:0,
-    color:"#2d8659",
-    colorBg:"#66cc99"
+    tools:"Javascript",
+    link:"https://www.gannett-cdn.com/-mm-/438112d08852a5cf64fb668899b62a1c6abcfadb/c=0-104-5312-3105&r=x1683&c=3200x1680/local/-/media/2017/05/23/WIGroup/Appleton/636311326049773956-UC.jpg",
+    pictures:['img/rewind-1.png','img/rewindcolors.png','img/rewindteaser.jpg'],
+    color:"#b3e6cc",
+    colorBg:"#2d8659",
+    bgOne:"linear-gradient( rgba(45, 134, 89,.9), rgba(45, 134, 89,.9)), url(img/rewind-bg.jpg) center no-repeat",
+    bgTwo:"linear-gradient( rgb(45, 134, 89,.9), rgb(45, 134, 89,.9)), url(img/rewind-bg.jpg) center no-repeat"
   },
   {
-    name:"Amazon",
-    content:0,
-    date:"30/01/17",
-    tools:0,
-    link:0,
-    content:0,
-    pictures:0,
-    color:"#e6b800",
-    colorBg:"#ffe066"
+    name:"Roadr",
+    content:"<p>It was the hardest project ever because we had never practiced on <b>PHP</b> and <b>MySql</b>! The goal of this project was to make a <b>Front office</b> content presentation of our choice ans a <b>Back office</b> of Content management system.</p></br>"+
+    "<p>Me and my friends chosed to create a blog on the technological advances of automotive, and ROADR was created.</p>",
+    date:"19/12/17",
+    tools:"php / mysql",
+    link:"https://github.com/metayerromain/groupe13",
+    pictures:['img/roadr-1.png','img/roadrcolors.png','img/roadrteaser.jpg'],
+    color:"#ffffff",
+    colorBg:"#02020b",
+    bgOne:"linear-gradient( rgba(2, 2, 11,.9), rgba(2, 2, 11,.9)), url(img/roader-bg.jpg) center no-repeat",
+    bgTwo:"linear-gradient( rgb(2, 2, 11,.9),rgb(2, 2, 11,.9)), url(img/roader-bg.jpg) center no-repeat"
   },
   {
-    name:"Erreur 404",
-    content:0,
+    name:"Error 404",
+    content:
+    "<p>_____ooo____ooo_o_______ooo_</p>"+
+    "<p>___oo_oo__oo___oo_____oo_oo_</p>"+
+    "<p>_oo___oo_oo___o_oo__oo___oo_</p>"+
+    "<p>oooooooo_oo__o__oo_oooooooo_</p>"+
+    "<p>______oo__ooo__oo________oo_</p>"+
+    "<p>______oo___oooo__________oo_</p>"+
+    "<p>__________o_________________</p>",
     date:"??/??/??",
-    tools:0,
-    link:0,
-    content:0,
-    pictures:0,
-    color:"#cc0000",
-    colorBg:"#ff3333"
+    tools:"? ? ?",
+    link:"https://www.youtube.com/watch?v=l1RM8ECQe5g",
+    pictures:['https://media.giphy.com/media/TqiwHbFBaZ4ti/giphy.gif','https://media.giphy.com/media/TqiwHbFBaZ4ti/giphy.gif','https://media.giphy.com/media/TqiwHbFBaZ4ti/giphy.gif'],
+    color:"#e4e4e4",
+    colorBg:"#121212",
+    bgTwo:"url(img/FlakyHighlevelInsect.gif) center repeat",
+    bgOne:"url(img/FlakyHighlevelInsect.gif) center repeat"
   }];
-var colorInit ="#838182 !important";
